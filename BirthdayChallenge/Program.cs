5810e6990ffc94fc5833247751263162c57fee10
David Reina <davidereina@hotmail.com>
2023-10-09 15:33:25 -0500

    Simple app that shows how many days have passed since the date you enter as input

---
+++ BirthdayChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BirthdayChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("Enter your birthday in this format: yyyy-mm-dd");
+            string birthday = Console.ReadLine();
+            DateTime birthdayDateTime;
+
+            if (DateTime.TryParse(birthday, out birthdayDateTime)) 
+            {
+                TimeSpan daysSinceBirthday = now.Subtract(birthdayDateTime);
+                Console.WriteLine($"Since your birthday ({birthday}) have passed {daysSinceBirthday.Days} days");
+            } else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+        }
+    }
+}
